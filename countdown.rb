a6ea1b9b5718bbc2be37eddf6f04aef6ea7a7885
Pat McClernan <patmccler@gmail.com>
2020-04-29 10:26:24 -0400

    add loop in countdown and add countdown_with_sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown time
+
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+
+
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep time
+  while time > 0
+   sleep(1)
+   puts "#{time} SECONDS!"
+   time -= 1
+  end
+
+  return "HAPPY NEW YEAR!"
+
 end
